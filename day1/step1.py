8b8a5de20a79b978f4210e144f83616e5da425f7
munishjainisro <79302835+munishjainisro@users.noreply.github.com>
2021-02-23 15:57:19 +0530

    Add files via upload

---
+++ day1/step1.py
@@ -0,0 +1,11 @@
+x=int(input('Enter value of x: '))
+y=int(input('Enter value of y: '))
+print('x+y:',x+y)
+print('x*y:',x*y)
+print('sum of x and y:',x+y)
+if x>=y :
+ print('x-y:',x-y)
+ print('x/y:',x/y)
+else : 
+ print('y-x:',y-x)
+ print('y/x:',y/x)
